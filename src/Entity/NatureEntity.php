ecf955082e15483246ef90d843d3666a50d8d86e
stophe <stophe72.d@gmail.com>
2020-04-17 08:49:22 +0200

    Ajout code nature

--- src/Entity/NatureEntity.php
+++ src/Entity/NatureEntity.php
@@ -4,12 +4,50 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\NatureRepository")
  * @ORM\Table(name="nature")
- * @UniqueEntity("libelle")
+ * @UniqueEntity("code")
  */
 class NatureEntity extends BaseLibelleEntity
 {
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank
+     *
+     * @ORM\Column(type="string", length=10, nullable=false)
+     */
+    private $code;
+
+    /**
+     * Get the value of code
+     *
+     * @return  string
+     */
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set the value of code
+     *
+     * @param  string  $code
+     *
+     * @return  self
+     */
+    public function setCode(string $code)
+    {
+        $this->code = strtoupper($code);
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->getCode() . ' - ' . $this->getLibelle();
+    }
 }
--- src/Form/NatureType.php
+++ src/Form/NatureType.php
@@ -3,10 +3,23 @@
 namespace App\Form;
 
 use App\Entity\NatureEntity;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class NatureType extends BaseLibelleType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add(
+                'code',
+                TextType::class
+            );
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
