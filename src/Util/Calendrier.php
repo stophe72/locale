9d1d8b19ad66c080d5c2312d235a3ca0f3ee5b15
stophe <stophe72.d@gmail.com>
2020-04-30 12:13:30 +0200

    Utilisation translation

--- src/Util/Calendrier.php
+++ src/Util/Calendrier.php
@@ -25,60 +25,30 @@ class Calendrier
     public function generate()
     {
         $date = new DateTime();
-        $calendrier = [];
+        $mois = [];
         for ($i = 1; $i <= 12; $i++) {
-            $mois = [];
+            $jours = [];
             $premierJour = date('N',  mktime(0, 0, 0, $i, 1, $this->annee));
             $nbJours = cal_days_in_month(CAL_GREGORIAN, $i, $this->annee);
             $j = 1;
             while ($j < $premierJour) {
-                $mois[] = new Jour();
+                $jours[] = new Jour();
                 $j++;
             }
             $j = 1;
             while ($j <= $nbJours) {
                 $date->setDate($this->annee, $i, $j);
-                $mois[] = new Jour($j, in_array($date->format('Y-m-d'), $this->visites));
+                $jours[] = new Jour($j, in_array($date->format('Y-m-d'), $this->visites));
                 $j++;
             }
-            $j = count($mois);
+            $j = count($jours);
             while ($j % 7 != 0) {
-                $mois[] = new Jour();
+                $jours[] = new Jour();
                 $j++;
             }
-            $calendrier[$this->getMoisName($i)] = $mois;
+            $mois[$i] = $jours;
         }
-        return $calendrier;
-    }
-
-    private function getMoisName(int $mois)
-    {
-        switch ($mois) {
-            case 1:
-                return  'Janvier';
-            case 2:
-                return  'Février';
-            case 3:
-                return  'Mars';
-            case 4:
-                return  'Avril';
-            case 5:
-                return  'Mai';
-            case 6:
-                return  'Juin';
-            case 7:
-                return  'Juillet';
-            case 8:
-                return  'Août';
-            case 9:
-                return  'Septembre';
-            case 10:
-                return  'Octobre';
-            case 11:
-                return  'Novembre';
-            default:
-                return  'Décembre';
-        }
+        return $mois;
     }
 
     /**
