72860685278512148629db67f402359ad4fdf0a4
stophe <stophe72.d@gmail.com>
2021-05-05 21:58:48 +0200

    20210505

--- src/Entity/MajeurEntity.php
+++ src/Entity/MajeurEntity.php
@@ -5,14 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\MajeurRepository")
  * @ORM\Table(name="majeur")
- * @UniqueEntity(fields={"groupeId", "nom", "prenom"})
  */
 class MajeurEntity extends BaseGroupeEntity
 {
